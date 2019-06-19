607141275d7ca22eb694f613c4aff43d9757450b
hikobe8 <hikobe8@163.com>
2019-06-19 16:08:38 +0800

    use condition in template

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World"
+    user = {'username': 'Ray'}
+    return render_template('index.html', user=user)
